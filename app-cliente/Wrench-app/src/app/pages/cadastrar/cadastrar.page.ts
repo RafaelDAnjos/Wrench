2300ab916ae6be32675f3329aa39ce7b3c32c18a
UserVert <rafael.anjos@vert.com.br>
2021-08-19 08:13:02 -0300

    :sparkles: integrando o back-end de login com o front-end

--- app-cliente/Wrench-app/src/app/pages/cadastrar/cadastrar.page.ts
+++ app-cliente/Wrench-app/src/app/pages/cadastrar/cadastrar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Local } from 'protractor/built/driverProviders';
+import { AutorizacaoService } from 'src/app/services/autorizacao.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar.page.scss'],
 })
 export class CadastrarPage implements OnInit {
-
-  constructor() { }
+  inputNome:string;
+  inputEmail:string;
+  inputIdentificacao:string;
+  inputSenha:string;
+  inputRepetirSenha:string;
+  constructor(private authService:AutorizacaoService, private toastCtrl: ToastController) { }
 
   ngOnInit() {
+  }
+
+  async Cadastrar(){
+    let usuario = {
+      nome: this.inputNome,
+      email: this.inputEmail,
+      senha: this.inputSenha,
+      confirmarSenha: this.inputRepetirSenha,
+      identificacao: this.inputIdentificacao,
+      tipoUsuario: 0
+    }
+    let response = await this.authService.cadastrar_usuario(usuario);
+    if(response.Sucesso){
+      localStorage.setItem('usuario_logado',response.Token);
+    }else{
+      let toast = await this.toastCtrl.create({
+        message: response.Errors[0],
+        duration: 1000,
+        position: 'top'
+      });
+      toast.present();
+    }
   }
 
 }
---
+++ app-prestador/Wrench-prestador/src/app/services/autorizacao.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutorizacaoService {
+  private url:any = 'https://localhost:44303';
+  constructor(private http:HttpClient) { }
+
+  logar(json:any):Promise<any> {
+  
+    return this.http.post(`${this.http}/api/auth/login`,json).toPromise();
+  
+  }
+
+  cadastrar_usuario(json:any):Promise<any> {
+
+    return this.http.post(`${this.url}/api/auth/registrar`,json).toPromise();
+
+  }
+
+
+}
