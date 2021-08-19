144f988914404f427ce5b037900ac585b8622f5f
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2021-08-18 22:38:48 -0300

    configuracao das entidades no banco de dados

---
+++ api/Wrench.Data/Configurations/TagConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Wrench.Domain.Entities;
+
+namespace Wrench.Data.Configurations
+{
+    public class TagConfiguration : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.HasKey(x => x.IdTag);
+
+            builder
+                .HasIndex(x => x.Nome)
+                .IsUnique();
+
+            builder
+                .Property(x => x.Nome)
+                .IsRequired();
+
+        }
+    }
+}
--- api/Wrench.Data/Migrations/20210819013706_entidades_basicas.Designer.cs
+++ api/Wrench.Data/Migrations/20210819013706_entidades_basicas.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Wrench.Data.Context;
 
 namespace Wrench.Data.Migrations
 {
     [DbContext(typeof(WrenchDbContext))]
-    partial class WrenchDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210819013706_entidades_basicas")]
+    partial class entidades_basicas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
