c0ea9d3ae6deb0f96277214523c4d66e7aeba634
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2021-08-20 16:58:41 -0300

    fix cors

--- api/Wrench.API/Startup.cs
+++ api/Wrench.API/Startup.cs
@@ -30,13 +30,7 @@ namespace Wrench.API
 
             services.AddIdentityJwt();
 
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(builder =>
-                {
-                    builder.AllowAnyOrigin();
-                });
-            });
+            services.AddCors();
 
             services.AddSwaggerGen(c =>
             {
@@ -76,6 +70,12 @@ namespace Wrench.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
 
             app.UseAuthentication();
             app.UseAuthorization();
