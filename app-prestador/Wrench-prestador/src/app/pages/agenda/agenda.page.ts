986709da180954b1c503ea8f2620af1791c4e27a
UserVert <rafael.anjos@vert.com.br>
2021-09-06 19:35:23 -0300

    :recycle: Refatorando os visuais da pagina da agenda, e preparando para a integração na parte do prestador

--- app-prestador/Wrench-prestador/src/app/pages/agenda/agenda.page.ts
+++ app-prestador/Wrench-prestador/src/app/pages/agenda/agenda.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { AlertController, NavController, ToastController } from '@ionic/angular';
+import { toastController } from '@ionic/core';
+import { format } from 'path';
 
 @Component({
   selector: 'app-agenda',
@@ -8,14 +10,99 @@ import { NavController } from '@ionic/angular';
 })
 export class AgendaPage implements OnInit {
 
-  constructor(private navCtrl:NavController) { }
+  constructor(private navCtrl:NavController, private alertCtrl:AlertController, private toastCtrl:ToastController) { }
 
   ngOnInit() {
   }
 
-  logout(){
+  async logout(){
     localStorage.removeItem('usuario_logado');
-    this.navCtrl.navigateForward('home');
+    await this.navCtrl.navigateForward('home');
+  }
+  async concluirServico(){
+    let alerta_conclusao = await this.alertCtrl.create({
+        header: 'Deseja Concluir esse serviço?',
+        inputs: [{
+          name: 'Valor',
+          type: 'text',
+          placeholder: 'Entre com o valor cobrado'
+  
+        },
+        {
+          name:'Prazo',
+          type: 'date',
+          placeholder: 'Indique a data de conclusão'
+        }],
+        buttons: [{
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel:');
+          }
+  
+        },
+        {
+          text: 'Adicionar',
+          handler: (form) => {
+            this.addConcluir(form);
+          }
+        }],
+      }
+    );
+    alerta_conclusao.present();
   }
 
+  addConcluir(form:any){
+    //integrar com o back-end
+  }
+  async avaliarDemandante(){
+    let alerta = await this.alertCtrl.create({
+      header: 'Por favor avalie o demandante',
+      inputs: [{
+        name: 'Nota',
+        type: 'number',
+        placeholder: 'Digite uma nota entre 0 e 10',
+      },
+      {
+        name:'Prazo',
+        type: 'date',
+        placeholder: 'Indique a data de conclusão'
+      }],
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Confirm Cancel:');
+        }
+
+      },
+      {
+        text: 'Adicionar',
+        handler: async (form) => {
+          if(form.nota<0 || form.nota>10){
+            let toast = await this.toastCtrl.create({
+              message:"Digite uma nota válida!",
+              position:'top',
+              duration: 2000
+            });
+            toast.present();
+          }else{
+            this.addAvaliacao(form);
+
+          }
+        }
+      }],
+    });
+    alerta.present();
+    this.concluirServico();
+  }
+  addAvaliacao(form:any){
+
+    // Integrar com o back-end
+  }
+  cancelarServico(){
+    //Integrar com o back-end
+  }
 }
