ebbc4879a0a0dce06d5a3c891139168aceef658f
UserVert <rafael.anjos@vert.com.br>
2021-09-07 14:43:34 -0300

    Ajustando front-end do cliente

--- app-cliente/Wrench-app/src/app/pages/agenda/agenda.page.ts
+++ app-cliente/Wrench-app/src/app/pages/agenda/agenda.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-agenda',
@@ -7,9 +8,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendaPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCtrl:AlertController, private toastCtrl:ToastController) { }
 
   ngOnInit() {
+  }
+  async concluirServico(){
+    let alerta_conclusao = await this.alertCtrl.create({
+        header: 'Deseja Concluir esse serviço?',
+        inputs: [{
+          name: 'Valor',
+          type: 'text',
+          placeholder: 'Entre com o valor cobrado'
+  
+        },
+        {
+          name:'Prazo',
+          type: 'date',
+          placeholder: 'Indique a data de conclusão'
+        }],
+        buttons: [{
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel:');
+          }
+  
+        },
+        {
+          text: 'Adicionar',
+          handler: (form) => {
+            this.addConcluir(form);
+          }
+        }],
+      }
+    );
+    alerta_conclusao.present();
+  }
+
+  addConcluir(form:any){
+    //integrar com o back-end
+  }
+  async avaliarPrestador(){
+    let alerta = await this.alertCtrl.create({
+      header: 'Por favor avalie o prestador',
+      inputs: [{
+        name: 'Nota',
+        type: 'number',
+        placeholder: 'Digite uma nota entre 0 e 10',
+      }],
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Confirm Cancel:');
+        }
+
+      },
+      {
+        text: 'Adicionar',
+        handler: async (form) => {
+          if(form.nota<0 || form.nota>10){
+            let toast = await this.toastCtrl.create({
+              message:"Digite uma nota válida!",
+              position:'top',
+              duration: 2000
+            });
+            toast.present();
+          }else{
+            this.addAvaliacao(form);
+
+          }
+        }
+      }],
+    });
+    alerta.present();
+    this.concluirServico();
+  }
+  addAvaliacao(form:any){
+
+    // Integrar com o back-end
+  }
+  cancelarServico(){
+    //Integrar com o back-end
   }
 
 }
